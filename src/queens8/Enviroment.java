12ec299abd6c84953bdf71b531855bd53a4e2987
davidxyz <hunpatind2@yahoo.com>
2015-09-30 21:25:24 -0500

    working hill climbing first choice and fixed steepest ascent 'first choice really inefficient'

--- src/queens8/Enviroment.java
+++ src/queens8/Enviroment.java
@@ -1,18 +1,22 @@
 package queens8;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.List;
 
+//getting successor states methods doesn't alter the Environment's state
 public class Enviroment {
 	private int[][] enviromentState = new int[8][8];
 	private int[][] initialState = new int[8][8];
-	int numQueens;
+	private Random rand;
+	int numQueens=0;
 	
 	public Enviroment(){
-		
+		rand = new Random();
 	}
 	public Enviroment(int[][] init,int[][] env){
 		initialState = init;
-		enviromentState = env;
+		enviromentState = Enviroment.deepCopyMatrix(env);
+		rand = new Random();
 		for(int i = 0; i < 8; i++)
 		   {
 		      for(int j = 0; j < 8; j++)
@@ -25,22 +29,21 @@ public class Enviroment {
 	}
 	public Enviroment(int[][] init,int[][] env,int num){
 		initialState = init;
-		enviromentState = env;
+		enviromentState = Enviroment.deepCopyMatrix(env);
 		numQueens = num;
+		rand = new Random();
 	}
 	public boolean setInitialState(String initialState){
 		boolean bResult = false;
 		int iIndex = 0;
-		int num=0;
 		for (int i = 0 ; i < initialState.length() ; i++){
 			if (Integer.parseInt(initialState.substring(iIndex, iIndex + 1)) > 0){
 				this.initialState[Integer.parseInt(initialState.substring(iIndex, iIndex + 1)) - 1][i] = 1;
-				num++;
+				numQueens++;
 			}
 			iIndex++;
 		}
-		numQueens = num;
-		this.enviromentState = this.initialState;
+		this.enviromentState = Enviroment.deepCopyMatrix(this.initialState);
 		bResult = true;
 		
 		return bResult;
@@ -69,16 +72,26 @@ public class Enviroment {
 		      System.out.println();
 		   }
 	}
-	public List<Enviroment> getSuccessorStates(){
-		List<Enviroment> successorStates = new ArrayList<Enviroment>();
-		int[][] successorEnvState = enviromentState.clone();
+	public static int[][] deepCopyMatrix(int[][] input) {
+	    if (input == null)
+	        return null;
+	    int[][] result = new int[input.length][];
+	    for (int r = 0; r < input.length; r++) {
+	        result[r] = input[r].clone();
+	    }
+	    return result;
+	}
+	public void printSuccessorStates(){
+		int[][] successorEnvState = enviromentState;
 		//don't have 8 queens on board
 		if(numQueens<8){
-			  for(int j = 0;j<8;j++){
-    			  successorEnvState[numQueens][j] = 1;
-    			  Enviroment env = new Enviroment(initialState,successorEnvState,numQueens+1);
-    			  successorStates.add(env);
-    			  successorEnvState[numQueens][j] = 0;
+			  for(int i = 0;i<8;i++){
+    			  successorEnvState[i][numQueens] = 1;
+    			  Enviroment env = new Enviroment(initialState,successorEnvState);
+    			  successorEnvState[i][numQueens] = 0;
+    			  env.printEnviromentState();
+    			  System.out.println("");
+    			  //successorEnvState[i][numQueens] = 0;
     		  }
 
 		}else{
@@ -92,10 +105,46 @@ public class Enviroment {
 		    		  successorEnvState[i][j] = 0;
 		    		  for(int n=1;n<8;n++){
 		    			  //set to 1 then 0 so as to restart the state for the successor
-		    			  successorEnvState[(i+n)%7][j] = 1;
+		    			  successorEnvState[(i+n)%8][j] = 1;
+		    			  Enviroment env = new Enviroment(initialState,successorEnvState);
+		    			  env.printEnviromentState();
+		    			  System.out.println("");
+		    			  successorEnvState[(i+n)%8][j] = 0;
+		    		  }
+		    		  successorEnvState[i][j] = 1;
+		    	  }
+		      }
+		      //successorEnvState = enviromentState.clone();
+		   }
+		}
+	}
+	public List<Enviroment> getSuccessorStates(){
+		List<Enviroment> successorStates = new ArrayList<Enviroment>();
+		int[][] successorEnvState = enviromentState;
+		//don't have 8 queens on board
+		if(numQueens<8){
+			  for(int i = 0;i<8;i++){
+    			  successorEnvState[i][numQueens] = 1;
+    			  Enviroment env = new Enviroment(initialState,successorEnvState);
+    			  successorStates.add(env);
+    			  successorEnvState[i][numQueens] = 0;
+    		  }
+
+		}else{
+		  for(int i = 0; i < 8; i++)
+		   {
+			  
+		      for(int j = 0; j < 8; j++)
+		      {
+		    	  //if we find a queen generate 7 successor states of the environment by moving the queen on that column
+		    	  if(enviromentState[i][j] == 1){
+		    		  successorEnvState[i][j] = 0;
+		    		  for(int n=1;n<8;n++){
+		    			  //set to 1 then 0 so as to restart the state for the successor
+		    			  successorEnvState[(i+n)%8][j] = 1;
 		    			  Enviroment env = new Enviroment(initialState,successorEnvState);
 		    			  successorStates.add(env);
-		    			  successorEnvState[(i+n)%7][j] = 0;
+		    			  successorEnvState[(i+n)%8][j] = 0;
 		    		  }
 		    		  successorEnvState[i][j] = 1;
 		    	  }
@@ -104,6 +153,44 @@ public class Enviroment {
 		   }
 		}
 		  return successorStates;
+	}
+	private int randInt(int min, int max){
+
+
+		    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+		    return randomNum;
+	}
+	public Enviroment getSuccessorStateRandomly(){
+		int[][] successorEnvState = enviromentState;
+		//don't have 8 queens on board
+		if(numQueens<8){
+    			  successorEnvState[randInt(0,7)][numQueens] = 1;
+    			  return new Enviroment(initialState,successorEnvState);
+
+		}else{
+			//is this a valid successor state. If queen is there it is not
+			//if no queen move queen here
+			int newColumnIndex=randInt(0,7),j=randInt(0,7);
+			int oldColumnIndex;
+			while(successorEnvState[newColumnIndex][j]==1){
+				newColumnIndex=randInt(0,7);
+				j=randInt(0,7);
+		    		  
+			}
+			for(oldColumnIndex=0;oldColumnIndex<8; oldColumnIndex++){
+				//found queen. Let's move it to generate successor state
+				if(successorEnvState[oldColumnIndex][j]==1){
+					successorEnvState[oldColumnIndex][j] = 0;
+					successorEnvState[newColumnIndex][j] =  1;
+					return new Enviroment(initialState,successorEnvState);
+					
+				}			    		  
+			}
+				
+		   
+		}
+		return null;//error. Shouldn't get here ever!
 	}
 	public int[][] getEnviromentState() {
 		return enviromentState;
@@ -119,6 +206,9 @@ public class Enviroment {
 
 	public void setInitialState(int[][] initialState) {
 		this.initialState = initialState;
+	}
+	public int getNumQueens() {
+		return numQueens;
 	}
 
 }
--- src/queens8/Queens8Driver.java
+++ src/queens8/Queens8Driver.java
@@ -9,12 +9,13 @@ import java.util.List;
 
 
 public class Queens8Driver {
-	final static boolean DEBUG = true;
+	final static boolean DEBUG = false;
 	public static void main(String[] args) {
 		List<Enviroment> gameList = new ArrayList<Enviroment>();
 		readGameFile(gameList);
 		gameList.get(0).printEnviromentState();
 		testHillClimbingSteepestAscent(gameList);
+		testHillClimbingFirstChoice(gameList);
 	}
 	public static boolean readGameFile(List<Enviroment> gameList) {
 		BufferedReader reader = null;
@@ -56,11 +57,26 @@ public class Queens8Driver {
 		for(int i=0;i<gameList.size();i++){
 			cost = hillClimbingSteepestAscent(gameList.get(i));
 			if(cost == 0){
+				
 				solved++;
 			}
 		}
-		System.out.printf("solved problems: %.2f\n",(solved/gameList.size())*100);
+		System.out.printf("(hill climbing steepest ascent): solved problems: %.2f\n",(solved/gameList.size())*100);
 	}
+	public static void testHillClimbingFirstChoice(List<Enviroment> gameList){
+		//hillClimbing steepest Ascent
+		int cost = 0;
+		float solved=0;
+		for(int i=0;i<gameList.size();i++){
+			cost = hillClimbingFirstChoice(gameList.get(i));
+			if(cost == 0){
+				
+				solved++;
+			}
+		}
+		System.out.printf("(hill climbing first choice): solved problems: %.2f\n",(solved/gameList.size())*100);
+	}
+	
 	/**
 	 * Non limiting steepest ascent which aborts if it found the perfect solution or a local minimum
 	 * @param env
@@ -70,11 +86,10 @@ public class Queens8Driver {
 		List<Enviroment> successorStates = env.getSuccessorStates();
 		int minHCost = Integer.MAX_VALUE;
 		Enviroment successorEnv = env; //start with current env
-		boolean foundSuccessor = true; //bool on whether the algoirthm can find a successor state with a lower cost
-		
+		boolean foundSuccessor = true; //bool on whether the algoirthim can find a successor state with a lower cost
 		//perfect solution minHCost = 0
 		//if we don't find a successor with a lower cost abort
-		while(minHCost!=0 && foundSuccessor){
+		while((minHCost!=0 && foundSuccessor)||successorEnv.getNumQueens()!=8){
 			foundSuccessor = false;
 			
 			for(int i=0; i<successorStates.size();i++){
@@ -85,13 +100,19 @@ public class Queens8Driver {
 					foundSuccessor = true;
 				}
 			}
+			//reset minHCost if we don't have 8 queens on the board
+			if(successorEnv.getNumQueens()!=8){
+				minHCost = Integer.MAX_VALUE;
+			}
+			
 			successorStates = successorEnv.getSuccessorStates();
 		
 		}
-		if(DEBUG&&minHCost == 0){
-			System.out.println("perfect solution was found for env with initial state:");
+		if(DEBUG&&minHCost==0){
+			System.out.printf("solved:\n");
+			System.out.printf("ENV init state:\n");
 			successorEnv.printInitialState();
-			System.out.println("perfect solution was:");
+			System.out.println("solution:\n");
 			successorEnv.printEnviromentState();
 		}
 		return minHCost;
@@ -107,11 +128,11 @@ public class Queens8Driver {
 					
 					for(int x = 1; x<8 ;x++){
 						//attack on row
-						if(enviromentState[i][(j+x)%7] == 1){
+						if(enviromentState[i][(j+x)%8] == 1){
 							hcost++;
 						}
 						//attack on column
-						if(enviromentState[(i+x)%7][j] == 1){
+						if(enviromentState[(i+x)%8][j] == 1){
 							hcost++;
 						}
 						//attack on diagonal
@@ -132,7 +153,45 @@ public class Queens8Driver {
 				}
 			}
 		}
+		if(DEBUG){
+			//System.out.println("Inside heuristic function with ENV:");
+			//env.printEnviromentState();
+			//System.out.printf("hcost is:%d\n",hcost);
+		}
 		return hcost;
+	}
+	//complete but inefficient
+	public static int hillClimbingFirstChoice(Enviroment env){
+		int minHCost = Integer.MAX_VALUE;
+		Enviroment successorEnv = env; //start with current env
+		Enviroment successorEnvContender= null;
+		
+		
+		//perfect solution minHCost = 0
+		//if we don't find a successor with a lower cost abort
+		while(minHCost!=0 || successorEnv.getNumQueens()!=8){
+
+			successorEnvContender = successorEnv.getSuccessorStateRandomly();
+			int curHCost = hueristicCost(successorEnvContender);
+			if(curHCost<minHCost){
+				minHCost = curHCost;
+				successorEnv = successorEnvContender;
+			}
+			//reset minHCost if we don't have 8 queens on the board
+			//because the minHCost doesn't count when less than 8 queens are on board
+			if(successorEnv.getNumQueens()!=8){
+				minHCost = Integer.MAX_VALUE;
+			}
+	}
+		
+		if(DEBUG&&minHCost==0){
+			System.out.printf("solved:\n");
+			System.out.printf("ENV init state:\n");
+			successorEnv.printInitialState();
+			System.out.println("solution:\n");
+			successorEnv.printEnviromentState();
+		}
+		return minHCost;
 	}
 	
 }
